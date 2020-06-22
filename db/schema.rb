95d6eaefc930ca1835a10e4e791de8a35d7105d9
André Perdigão <andrepcg@gmail.com>
2020-06-22 01:57:46 +0100

    Add dockerfile

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,62 +10,63 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_200_620_181_413) do
+ActiveRecord::Schema.define(version: 2020_06_20_181413) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'active_storage_attachments', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'record_type', null: false
-    t.bigint 'record_id', null: false
-    t.bigint 'blob_id', null: false
-    t.datetime 'created_at', null: false
-    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
-    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table 'active_storage_blobs', force: :cascade do |t|
-    t.string 'key', null: false
-    t.string 'filename', null: false
-    t.string 'content_type'
-    t.text 'metadata'
-    t.bigint 'byte_size', null: false
-    t.string 'checksum', null: false
-    t.datetime 'created_at', null: false
-    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table 'domains', force: :cascade do |t|
-    t.string 'domain'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "domains", force: :cascade do |t|
+    t.string "domain"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table 'inbound_emails', force: :cascade do |t|
-    t.bigint 'mailbox_id', null: false
-    t.text 'text'
-    t.text 'html'
-    t.string 'from'
-    t.string 'name'
-    t.string 'sender_ip'
-    t.string 'subject'
-    t.text 'headers'
-    t.datetime 'read_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['created_at'], name: 'index_inbound_emails_on_created_at'
-    t.index ['mailbox_id'], name: 'index_inbound_emails_on_mailbox_id'
+  create_table "inbound_emails", force: :cascade do |t|
+    t.bigint "mailbox_id", null: false
+    t.text "text"
+    t.text "html"
+    t.string "from"
+    t.string "name"
+    t.string "sender_ip"
+    t.string "subject"
+    t.text "headers"
+    t.datetime "read_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["created_at"], name: "index_inbound_emails_on_created_at"
+    t.index ["mailbox_id"], name: "index_inbound_emails_on_mailbox_id"
   end
 
-  create_table 'mailboxes', force: :cascade do |t|
-    t.string 'email'
-    t.string 'token'
-    t.datetime 'expires_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['email'], name: 'index_mailboxes_on_email', unique: true
+  create_table "mailboxes", force: :cascade do |t|
+    t.string "email"
+    t.string "token"
+    t.datetime "expires_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_mailboxes_on_email", unique: true
   end
 
-  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'inbound_emails', 'mailboxes'
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "inbound_emails", "mailboxes"
 end
